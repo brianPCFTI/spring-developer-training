1b73a282bbb3f6f943d233a8c3d57e4ad0b70f82
Brian Mora <brian.mora@pfcti.com>
2023-03-21 17:38:52 -0600

    actualizan las consultas en los repository

--- spring-data/src/main/java/com/pfcti/springdata/repository/CuentaRepository.java
+++ spring-data/src/main/java/com/pfcti/springdata/repository/CuentaRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CuentaRepository extends JpaRepository<Cuenta, Integer>, JpaSpecificationExecutor<Cuenta> {
-
+    List<Cuenta> findByCliente_IdAndEstadoIsTrue(int clienteId);
     void deleteAllByCliente_Id(int clienteId);
 }
--- spring-data/src/main/java/com/pfcti/springdata/repository/TarjetaRepository.java
+++ spring-data/src/main/java/com/pfcti/springdata/repository/TarjetaRepository.java
@@ -1,6 +1,7 @@
 package com.pfcti.springdata.repository;
 
 import com.pfcti.springdata.model.Cliente;
+import com.pfcti.springdata.model.Cuenta;
 import com.pfcti.springdata.model.Tarjeta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,7 @@ import java.util.List;
 
 @Repository
 public interface TarjetaRepository extends JpaRepository<Tarjeta, Integer> {
+
+    List<Tarjeta> findByCliente_IdAndEstadoIsTrue(int clienteId);
 
 }
