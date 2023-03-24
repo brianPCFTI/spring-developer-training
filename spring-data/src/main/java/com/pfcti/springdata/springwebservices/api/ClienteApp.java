6921cd6f3344cf4f8258fc029664beb9077d5943
Brian Mora <brian.mora@pfcti.com>
2023-03-23 18:06:41 -0600

    api rest uso de parametros en query string

--- spring-data/src/main/java/com/pfcti/springdata/springwebservices/api/ClienteApp.java
+++ spring-data/src/main/java/com/pfcti/springdata/springwebservices/api/ClienteApp.java
@@ -4,10 +4,8 @@ import com.pfcti.springdata.dto.ClienteDto;
 import com.pfcti.springdata.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/api/cliente")
@@ -23,6 +21,32 @@ public class ClienteApp {
     public ClienteDto buscarCliente(@PathVariable int id){
         log.info("Busqueda del cliente: {} ", id);
         return clienteService.obtenerCliente(id);
+    }
+
+    @GetMapping(value = "/xml/{id}", produces = MediaType.APPLICATION_XML_VALUE)
+    public ClienteDto buscarClienteXml(@PathVariable int id){
+        log.info("Busqueda del cliente: {} ", id);
+        return clienteService.obtenerCliente(id);
+    }
+
+    @GetMapping(value = "/xml/json/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ClienteDto buscarClienteXmlJson(@PathVariable int id){
+        log.info("Busqueda del cliente: {} ", id);
+        return clienteService.obtenerCliente(id);
+    }
+
+    //Variables por parametro
+    //http://localhost:8080/v1/api/cliente/xml/json/1?parameter=1
+    @GetMapping(value = "/parameter", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ClienteDto buscarPorParametro(@RequestParam int id){
+        log.info("Busqueda del cliente: {} ", id);
+        return clienteService.obtenerCliente(id);
+    }
+
+    @PostMapping
+    public void guardarCliente (@RequestBody ClienteDto clienteDto){
+        log.info("cliente de cliente: {} ", clienteDto);
+        clienteService.insertarCliente(clienteDto);
     }
 
 }
