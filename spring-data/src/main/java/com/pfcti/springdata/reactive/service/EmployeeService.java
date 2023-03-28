95c8cab123ee4e671e4e5b706e198fa014b553a9
Brian Mora <brian.mora@pfcti.com>
2023-03-28 16:24:03 -0600

    webflux - api reactivo

---
+++ spring-data/src/main/java/com/pfcti/springdata/reactive/service/EmployeeService.java
@@ -0,0 +1,32 @@
+package com.pfcti.springdata.reactive.service;
+
+import com.pfcti.springdata.reactive.model.Employee;
+import com.pfcti.springdata.reactive.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepo;
+    public void create(Employee e) {
+        employeeRepo.save(e).subscribe();
+    }
+    public Mono<Employee> findById(Integer id) {
+        return employeeRepo.findById(id);
+    }
+    public Flux<Employee> findByName(String name) {
+        return employeeRepo.findByName(name);
+    }
+    public Flux<Employee> findAll() {
+        return employeeRepo.findAll();
+    }
+    public Mono<Employee> update(Employee e) {
+        return employeeRepo.save(e);
+    }
+    public Mono<Void> delete(Integer id) {
+        return employeeRepo.deleteById(id);
+    }
+}
