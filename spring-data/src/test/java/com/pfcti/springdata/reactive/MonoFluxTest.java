fc2e06d7b38f4e0f05cb87dcffea135a4a15e0c3
Brian Mora <brian.mora@pfcti.com>
2023-03-27 19:03:40 -0600

    webflux

---
+++ spring-data/src/test/java/com/pfcti/springdata/reactive/MonoFluxTest.java
@@ -0,0 +1,45 @@
+package com.pfcti.springdata.reactive;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class MonoFluxTest {
+
+    @Test
+    public void givenMonoPublisher_whenSubscribeThenReturnSingleValue(){
+        Mono<String> helloMono = Mono.just("Hola");
+        StepVerifier.create(helloMono)
+                .expectNext("Hola") .expectComplete() .verify();
+    }
+
+    @Test
+    public void givenFluxPublisher_whenSubscribedThenReturnMultipleValues(){
+        Flux<String> stringFlux = Flux.just("Hola", "Spring Reactive");
+        StepVerifier.create(stringFlux)
+                .expectNext("Hola")
+                .expectNext("Spring Reactive")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void givenFluxPublisher_whenSubscribeThenReturnMultipleValuesWithError(){
+        Flux<String> stringFlux = Flux.just("Hola", "Spring Reactive", "Error")
+                .map(str -> {
+                    if (str.equals("Error"))
+                        throw new RuntimeException("Throwing Error");
+                    return str; });
+
+        StepVerifier
+                .create(stringFlux)
+                .expectNext("Hello")
+                .expectNext("Spring course")
+                .expectError()
+                .verify();
+    }
+
+}
