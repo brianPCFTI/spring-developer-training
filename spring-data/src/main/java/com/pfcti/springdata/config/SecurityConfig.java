ca6b46b834c55df8823253bd3846f454d95b69fd
Brian Mora <brian.mora@pfcti.com>
2023-03-30 17:33:13 -0600

    spring security

--- spring-data/src/main/java/com/pfcti/springdata/config/SecurityConfig.java
+++ spring-data/src/main/java/com/pfcti/springdata/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     /*
